1312d8726e2d1c8b963b94caf81468ab0825fd0e
lanloptele <100698914+lanloptele@users.noreply.github.com>
2022-03-29 13:17:29 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+                       
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Podaj imie: ");
+        String imie = sc.next();
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = sc.next();
+        
+
+        sc.close();
+
+    }
+    
+}

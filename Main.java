f4de03790a9321455894b96d451d91bbf1bbcb85
lanloptele <100698914+lanloptele@users.noreply.github.com>
2022-04-08 14:47:48 +0000

    działa

--- Main.java
+++ Main.java
@@ -3,17 +3,31 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
-                       
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Podaj imie: ");
-        String imie = sc.next();
-        System.out.print("Podaj nazwisko: ");
-        String nazwisko = sc.next();
-        
-
-        sc.close();
-
-    }
+      System.out.print("Podaj słowo:\n");
+      Scanner sc=new Scanner(System.in);
+      String naszeslowo=sc.nextLine();
+      String wynik="";
+      char znak;
+      int kod;
+      int licznik=0;
+      for(int w=0;w<naszeslowo.length();w++){
+        znak=naszeslowo.charAt(w);
+        kod=(int)znak;
+        if(kod>=97&&kod<=122){//&& iloczyn logiczny
+          kod=kod-32;
+          znak=(char)kod;
+          ;
+        }
+        else{
+          if(kod>=65&&kod<=90){
+            kod=kod+32;
+            znak=(char)kod;
+            licznik++;
+          }}
+        wynik=wynik+znak;
+        }
     
+  System.out.print("Słowo wygląda nastepujaco: " +wynik +"\nIlość poprawek wynosi: " +licznik);
+    }
 }
+
